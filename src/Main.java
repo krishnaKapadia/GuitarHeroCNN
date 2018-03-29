bb7bc2860e31bc3be73118fab1857b983c38856e
KrishnaKapadia <kapadikris@myvuw.ac.nz>
2018-03-29 23:26:59 +1300

    Optimized video to speed up detections

--- src/Main.java
+++ src/Main.java
@@ -6,57 +6,74 @@ import org.opencv.videoio.VideoCapture;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.filechooser.FileSystemView;
+import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.io.ByteArrayInputStream;
-import java.io.File;
-import java.io.InputStream;
+import java.awt.image.DataBufferByte;
+import java.io.*;
+import java.util.Vector;
 
 /**
  * Runs the AI that can learn to play guitar hero
  * Created by krishna kapadia on 28/03/2018.
  */
 public class Main {
+    JFrame frame = new JFrame();
 
     public Main() {
 //        Step 1: use open cv to detect the screen edges
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-//        System.out.println(this.getClass().getProtectionDomain());
-        JFileChooser jfc = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
-        jfc.setDialogTitle("Choose a directory to save your file: ");
-        jfc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
 
-        int returnValue = jfc.showSaveDialog(null);
-        if (returnValue == JFileChooser.APPROVE_OPTION) {
-            if (jfc.getSelectedFile().isDirectory()) {
-                System.out.println("You selected the directory: " + jfc.getSelectedFile());
+        Rectangle screenRect = new Rectangle(600, 600);
+
+        while (true) {
+            try {
+//                Reads screen and passes the output to open cv
+                BufferedImage capture = new Robot().createScreenCapture(screenRect);
+                Mat color = convertImageToMat(capture);
+                System.out.println(color);
+                Mat gray = new Mat();
+                Mat draw = new Mat();
+                Mat edges = new Mat();
+                Imgproc.cvtColor(color, gray, Imgproc.COLOR_BGR2GRAY);
+                Imgproc.Canny(gray, edges, 50, 150, 3, false);
+                Mat circles = new Mat();
+                Vector<Mat> circlesList = new Vector<Mat>();
+
+                Imgproc.HoughCircles(edges, circles, Imgproc.CV_HOUGH_GRADIENT, 1, 60, 200, 20, 30, 0);
+
+
+
+                edges.convertTo(draw, CvType.CV_8U);
+
+
+
+//                Display the output to the JFrame
+                showResult(draw);
+            } catch (AWTException | IOException e) {
+                e.printStackTrace();
             }
         }
-//            jfc.getSelectedFile().toString() + File.separator + "testImage.jpg"
-//        VideoCapture videoCapture =
 
-        Mat color = Imgcodecs.imread(jfc.getSelectedFile().toString() + File.separator + "testImage.jpg");
-        System.out.println(color);
-        Mat gray = new Mat();
-        Mat draw = new Mat();
-        Mat wide = new Mat();
-        Imgproc.cvtColor(color, gray, Imgproc.COLOR_BGR2GRAY);
-        Imgproc.Canny(gray, wide, 50, 150, 3, false);
-        wide.convertTo(draw, CvType.CV_8U);
+    }
 
-//        if(Imgcodecs.imwrite("", draw)) {
-//            System.out.println("edge detected");
-//        }
-//
-//        Mat mat = Mat.eye(3, 3, CvType.CV_8UC1);
-//        System.out.println("mat = " + mat.dump());
-        showResult(draw);
+    /**
+     * Convert  buffered image object to a Mat so tha open cv can read it.
+     * @param image, to convert
+     * @return a Mat representing the passed in buffered image
+     * @throws IOException
+     */
+    public static Mat convertImageToMat(BufferedImage image) throws IOException {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        ImageIO.write(image, "jpg", byteArrayOutputStream);
+        byteArrayOutputStream.flush();
+        return Imgcodecs.imdecode(new MatOfByte(byteArrayOutputStream.toByteArray()), Imgcodecs.CV_LOAD_IMAGE_UNCHANGED);
     }
 
     /**
      * Shows the image in a new window
      * @param img, image to display
      */
-    public static void showResult(Mat img) {
+    public void showResult(Mat img) {
         Imgproc.resize(img, img, new Size(640, 480));
         MatOfByte matOfByte = new MatOfByte();
         Imgcodecs.imencode(".jpg", img, matOfByte);
@@ -65,7 +82,7 @@ public class Main {
         try {
             InputStream in = new ByteArrayInputStream(byteArray);
             bufImage = ImageIO.read(in);
-            JFrame frame = new JFrame();
+            frame.getContentPane().removeAll();
             frame.getContentPane().add(new JLabel(new ImageIcon(bufImage)));
             frame.pack();
             frame.setVisible(true);
@@ -76,7 +93,7 @@ public class Main {
 
 
 //    THIS METHOD IS A LOT FASTER BUT REQUIRES US TO DRAW IT TO A GRAPHICS OBJECT
-//    public Image toBufferedImage(Mat m){
+//    public BufferedImage toBufferedImage(Mat m){
 //        int type = BufferedImage.TYPE_BYTE_GRAY;
 //        if ( m.channels() > 1 ) {
 //            type = BufferedImage.TYPE_3BYTE_BGR;
@@ -88,7 +105,6 @@ public class Main {
 //        final byte[] targetPixels = ((DataBufferByte) image.getRaster().getDataBuffer()).getData();
 //        System.arraycopy(b, 0, targetPixels, 0, b.length);
 //        return image;
-//
 //    }
 
 
